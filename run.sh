1e39ba98c36febf90a659aa567ebe480f785d854
Linmyat14 <linmyat.n3@gmail.com>
2024-02-11 16:27:54 +0630

    completed project

---
+++ run.sh
@@ -0,0 +1,5 @@
+# sass Sass/App.scss CSS/App.css --style=compressed --no-source-map --watch
+
+# sass Sass/skill.scss CSS/skill.css --style=compressed --no-source-map --watch
+
+sass Sass/profile.scss CSS/profile.css --style=compressed --no-source-map --watch
